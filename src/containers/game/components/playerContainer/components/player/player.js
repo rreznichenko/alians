569ffeeb46599eaac620b02bad39b85933790049
Raybeedrill <demezis1994@gmal.com>
2019-03-24 04:37:31 +0200

    player style finished

--- src/containers/game/components/playerContainer/components/player/player.js
+++ src/containers/game/components/playerContainer/components/player/player.js
@@ -5,11 +5,15 @@ class Player extends Component {
     render() {
         return (
             <div className="player">
-                <input className="player__radio" type="radio" name="player" value={this.props.player.id} onChange={() => {this.props.onChooseHandler(this.props.player.id)}} /> 
-                <p className="player__name">name:{this.props.player.name}</p>
-                <button className="btn player_btn" onClick={() => {this.props.onRemoveHandler(this.props.player.id)}}> - </button>
-                <span>{this.props.player.score}</span>
-                <button onClick={() => {this.props.onAddHandler(this.props.player.id)}}> + </button>                
+                <div>
+                    <input className="player__radio" type="radio" name="player" value={this.props.player.id} onChange={() => {this.props.onChooseHandler(this.props.player.id)}} /> 
+                    <p className="player__name">name:{this.props.player.name}</p>
+                </div>
+                <div>
+                    <button className="btn player_btn" onClick={() => {this.props.onRemoveHandler(this.props.player.id)}}> - </button>
+                    <span className="player__score">{this.props.player.score}</span>
+                    <button className="btn player_btn" onClick={() => {this.props.onAddHandler(this.props.player.id)}}> + </button>                
+                </div>
             </div>
         )
     }
